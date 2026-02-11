50432528c35160811d8ee3a44df4a649c62fcc47
Pasham Varshitha <varshithapasham552@gmail.com>
2026-02-11 21:36:06 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,12 @@
+package methodoverriding;
+
+public class main {
+	public static void main(String[] args) 
+	{
+		Parent obj1 = new Parent();
+		obj1.show();
+		Parent obj2 = new Parent();
+		obj2.show();
+	}
+
+}
